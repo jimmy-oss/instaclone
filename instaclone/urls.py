3ae868e53507370fd28ab08cfd39a46d32feb2af
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-04 23:16:11 +0300

    url routing

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,6 +1,6 @@
-from operator import index
-from django .urls import path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-     path('',views.index, name=index)
+    path('', views.index, name='index'),
 ]
