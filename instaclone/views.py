a021cb9f07575920dd09604f055564d28b2add70
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-05 20:24:48 +0300

    signing users

--- instaclone/views.py
+++ instaclone/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index (request):
@@ -21,12 +22,19 @@ def signup(request):
             elif User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Taken')
                 return redirect('signup')
-         # Log user in and redirects to settings page
-         # Create a profile object for the new user
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
-            
+
+                #log user in and redirect to settings page
+                #user_login = auth.authenticate(username=username, password=password)
+                #auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
