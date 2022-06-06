75baf422947c195e1a286a33684b164652e95c43
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-06 19:04:13 +0300

    welcome email

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from .email import send_welcome_email
+
 
 # Create your views here.
 @login_required(login_url='signin')
@@ -29,14 +31,14 @@ def signup(request):
                 user.save()
 
                 #log user in and redirect to settings page
-                #user_login = auth.authenticate(username=username, password=password)
-                #auth.login(request, user_login)
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
@@ -64,6 +66,36 @@ def signin(request):
         return render(request, 'signin.html')
  
 @login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
+ 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+ 
